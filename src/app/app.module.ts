4823f979f5332c4622d0bd23e69781f4b343f45c
Lord17Charly <gutierrezvottecarlos@gmail.com>
2023-09-11 16:47:09 -0600

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { WorkExperienceComponent } from './work-experience/work-experience.component';
+import { WorkExperienceService } from './services/work-experience.service';
+
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -12,9 +16,13 @@ import { WorkExperienceComponent } from './work-experience/work-experience.compo
     WorkExperienceComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    WorkExperienceService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
