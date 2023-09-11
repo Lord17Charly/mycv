c113c66715351e979f5587a2e4250e76961c669d
Lord17Charly <gutierrezvottecarlos@gmail.com>
2023-09-11 15:29:46 -0600

    update

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeaderService } from '../services/header.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
 
+export class HeaderComponent implements OnInit{
+  constructor(private headerService : HeaderService) {}
+
+  name: string = "fake name"
+  titulos: string = "Tecnico en Programacion"
+  mision: string = "Poder trabajar y serle de ayuda a la empresa"
+  foto: string = "fake photo"
+  email: string = "yadhirTV@gmail.com"
+  cel: string = "(229) 968 5317";
+  location: string = "Orizaba, Ver."
+  social: string = "@yodoeaofi06"
+
+  ngOnInit() : void {
+    this.headerService.getHeaders()
+    .subscribe( (data: any) =>  {
+      console.log(data);
+      alert(data)
+
+      this.name = data.name;
+      this.titulos = data.titulos;
+      this.mision = data.mision;
+      this.foto = data.foto;
+      this.email = data.email;
+      this.cel = data.cel;
+      this.location = data.location;
+      this.social = data.social;
+    })
+    
+  }
 }
+
+
