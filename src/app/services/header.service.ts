491b03de9a080e8bda8015aaba65b44a0fcc6adf
Lord17Charly <gutierrezvottecarlos@gmail.com>
2023-09-14 20:03:20 -0600

    fixes

--- src/app/services/header.service.ts
+++ src/app/services/header.service.ts
@@ -2,39 +2,41 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { retry, catchError } from 'rxjs/operators';
-import { BehaviorSubject } from 'rxjs/';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeaderService {
 
-  constructor(private http: HttpClient) { }
   apiURL = 'http://localhost:8080/header';
+  constructor(private http: HttpClient) { }
 
-  //Http Options
   httpOptions = {
-	  headers: new HttpHeaders({
-		  'Content-Type' : 'application/json'
-	  })
-}
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin':'*'
+    })
+  }
 
-getHeader(): Observable<any> {
-	return this.http.get<any>(this.apiURL, this.httpOptions)
-	.pipe(
-		retry(1),
-		catchError(this.handleError)
-	)
-}
+  getHeader(): Observable<any> {
+    return this.http.get<any>(this.apiURL, this.httpOptions)
+    .pipe(
+      retry(1),
+      catchError(this.handleError)
+    )
+  }
 
-handleError(error: any) {
-	let errorMessage = '';
-	if (error.error instanceof ErrorEvent) {
-	errorMessage = error.error.message;
-	} else {
-		errorMessage = `Error code: ${error.status} \n Message: ${error.message}`
-	}
-	window.alert(errorMessage);
-	return throwError(errorMessage);
-}
+  handleError(error: any) {
+    let errorMessage = '';
+    if(error.error instanceof ErrorEvent) {
+      errorMessage = error.error.message;
+    }
+    else {
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+  }
+  
 }
