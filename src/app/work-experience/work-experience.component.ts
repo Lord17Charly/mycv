b76a492b8dd61ea4d633335dae1ac74128ddddbe
Lord17Charly <125098769+Lord17Charly@users.noreply.github.com>
2023-09-14 20:17:06 -0600

    WorkExperience (#12)
    * update develop
    
    * update
    
    * update
    
    * Update workexp
    
    * karma
    
    * karma
    
    * fixes
    
    * fixes
    
    * fixes
    
    * karma

--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WorkExperienceService } from '../services/work-experience.service';
 
 @Component({
   selector: 'app-work-experience',
   templateUrl: './work-experience.component.html',
   styleUrls: ['./work-experience.component.css']
 })
-export class WorkExperienceComponent {
+export class WorkExperienceComponent implements OnInit {
 
+  WorkExperience: any[] = [];
+
+    constructor(private workexperienceService: WorkExperienceService) {}
+    
+  ngOnInit() : void {
+    this.workexperienceService.getWorkExperience().subscribe((data: any[]) =>  {
+      console.log(data);
+      alert(data)
+
+      // Asignar los datos al array trabajo
+      this.WorkExperience = data;
+  });
+}
 }
